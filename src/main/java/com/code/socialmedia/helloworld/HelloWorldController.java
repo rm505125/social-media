1a40c19fb66dcd9344b3734138e7955ebdb9e81d
Rahul Maurya <rm505125@gmail.com>
2023-01-09 13:09:48 +0530

    content negotiation

--- src/main/java/com/code/socialmedia/helloworld/HelloWorldController.java
+++ src/main/java/com/code/socialmedia/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.code.socialmedia.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +28,19 @@ public class HelloWorldController {
 	@GetMapping("/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean("Hello World  " + name);
+	}
+	
+	
+	private MessageSource messageSource;
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource=messageSource;
+	}
+	
+	
+	@GetMapping("/hello-world-internationalize")
+	public String helloWorldInternationalize() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null,"Degault Message",locale);
+		//return "Hello World V2";
 	}
 }
